d8f4a00ca8eb861aa7b2f52f5e2f6b07e1310ef0
ABSO <atargaming901@gmail.com>
2026-01-09 02:10:42 +0300

    Add servers API endpoint with sample data

---
+++ api/servers.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+?>{
+    "success": true,
+    "servers": [
+        {
+            "id": 4334,
+            "name": "01. ʟ̤ɾʅɹɹɹɹ̣ـc",
+            "region": "peru",
+            "serverUrl": "wss://fra-c.wormate.io:32064/wormy",
+            "status": 1
+        },
+        {
+            "id": 7210,
+            "name": "02. (III",
+            "region": "peru",
+            "serverUrl": "wss://fra-c.wormate.io:32285/wormy",
+            "status": 1
+        }
+    ]
+}
