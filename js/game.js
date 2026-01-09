28930a33776056e8e4d655f04a0f2ef95031c65d
ABSO <atargaming901@gmail.com>
2026-01-09 19:18:38 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -144,7 +144,7 @@ let servers = {
 
 
 async function loadUsers() {
-    await fetch("https://wormps.github.io/extension/api/ nusers.php")
+    await fetch("https://wormps.github.io/extjension/api/ nusers.php")
         .then(response => response.json())
         .then(response => {
             if (response.success) {
